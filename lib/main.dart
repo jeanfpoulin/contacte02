10290c61943a063b2c025cc2ec41ec7524044caa
Mr. Poulin <jeanfpoulin@gmail.com>
2021-11-03 17:11:52 -0400

    change le texte

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(body: Text('Projet Vide')),
+      home: Scaffold(body: Text('Projet pour contactes')),
     );
   }
 }
